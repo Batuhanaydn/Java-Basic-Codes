83e7448d81623573e63b0209acdacb1e9d25e2f0
Muhammed Batuhan Aydın <56546166+Batuhanaydn@users.noreply.github.com>
2020-02-29 12:42:16 +0300

    create_triangle

---
+++ codes.v1/create_triangle.java
@@ -0,0 +1,11 @@
+public class create_triangle {
+    public static void main(String[] args) {
+        int rows = 10;
+        for(int i = 1;i <= rows; i++){
+            for(int a = 1; a <= i; a++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
